65d0a9e07847a1061235568a8ce8dc9e4ab4f92f
Guritchi-ru <kononovb287@gmail.com>
2026-05-02 08:59:09 +0700

    Выполнены первые 6 пунктов

--- lab16_threads/task6.c
+++ lab16_threads/task6.c
@@ -3,36 +3,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-// void* sleep_and_print(void* arg) {
-//     int n = *((int*)arg);
-//     usleep(n*10000);
-//     printf("%d ", n);
-//     return NULL;
-// }
-
-// int main() {
-//     int arr[] = {1, 33, 45, 6, 7, 9, 78, 2, 11, 14, 15, 19, 20, 1, 45, 78, 99, 34, 67, 99};
-//     //int arr[] = {4, 1, 2, 5, 3};
-//     int n = sizeof(arr) / sizeof(arr[0]);
-//     pthread_t threads[n];
-
-//     printf("Исходный массив: ");
-//     for (int i = 0; i < n; i++) {
-//         printf("%d ", arr[i]);
-//     }
-//     printf("\n");
-//     printf("Отсортированный массив: ");
-
-//     for (int i = 0; i < n; i++){
-//         pthread_create(&threads[i], NULL, sleep_and_print, &arr[i]);
-//     }
-
-//     for (int i = 0; i < n; i++){
-//         pthread_join(threads[i], NULL);
-//     }
-//     printf("\n");
-// }
-
 typedef struct {
     int value;
     int sleep;
